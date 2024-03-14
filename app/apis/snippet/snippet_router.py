6a2e2a7a0cd171d08c0372c8021eea67a9ba2bde
kmelon55 <kimkyoungmo2869@gmail.com>
2024-03-14 19:55:31 +0900

    Feat: Add lambda function CRUD apis

--- app/apis/snippet/snippet_router.py
+++ app/apis/snippet/snippet_router.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 from starlette.config import Config
 
 
-from apis.snippet.snippet_service import create_lambda_function 
+from apis.snippet.snippet_service import create_lambda_function, create_lambda_function_url, add_lambda_function_permission, delete_lambda_function, delete_lambda_function_url, update_lambda_function_code, update_lambda_function_configuration
+from apis.snippet.snippet_schema import Snippet_deploy_request, Snippet_delete_request, Snippet_update_request
 
 router = APIRouter(
     prefix="/snippet",
@@ -11,14 +12,41 @@ router = APIRouter(
 )
 
 @router.post("/deploy")
-def deploy_snippet(request):
+def deploy_lambda_function(request: Snippet_deploy_request):
 
-    function_name = 'test'
-    script = 'print(1+1)'
-    user_env_vars = {
-       'Variables': 'test'
-    }
+    function_name = request.function_name
+    script = request.script
+    user_env_vars = request.user_env_vars
 
-    response = create_lambda_function(function_name, script, user_env_vars)
+    lambda_function_response = create_lambda_function(function_name, script, user_env_vars)
+    create_lambda_function_url(function_name)
+    add_lambda_function_permission(function_name)
 
+    return lambda_function_response,
+
+
+@router.post("/update")
+def update_lambda_function(request: Snippet_update_request):
+
+    function_name = request.function_name
+    script = request.script
+    user_env_vars = request.user_env_vars
+
+    if script:
+        update_lambda_function_code_response = update_lambda_function_code(function_name, script)
+    
+    if user_env_vars:
+        update_lambda_function_configuration_response = update_lambda_function_configuration(function_name, user_env_vars)
+
+    return update_lambda_function_code_response, update_lambda_function_configuration_response
+
+
+@router.post("/delete")
+def delete_snippet(request: Snippet_delete_request):
+
+    function_name = request.function_name
+
+    delete_lambda_function_url
+    response = delete_lambda_function(function_name)
     return response
+
--- app/apis/snippet/snippet_service.py
+++ app/apis/snippet/snippet_service.py
@@ -1,18 +1,164 @@
 import boto3
+import zipfile
+import io
 
-lambda_client = boto3.client('lambda')
+from dotenv import load_dotenv
+from starlette.config import Config
 
-def create_lambda_function(function_name, script, user_env_vars):
-    response = lambda_client.create_function(
+load_dotenv()
+config = Config('.env')
+
+
+http_handler_code_template = """
+import json
+    http_method = event['requestContext']['http']['method']
+    
+    if http_method == 'GET':
+        # GET 요청 처리 로직
+        response = handle_get_request(event)
+        
+    elif http_method == 'POST':
+        # POST 요청 처리 로직
+        response = handle_post_request(event)
+        
+    elif http_method == 'PUT':
+        # PUT 요청 처리 로직
+        response = handle_put_request(event)
+        
+    elif http_method == 'DELETE':
+        # DELETE 요청 처리 로직
+        response = handle_delete_request(event)
+        
+    else:
+        response = {
+            'statusCode': 405,
+            'body': json.dumps('Method Not Allowed')
+        }
+"""
+
+def create_zip_file(code):
+    zip_output = io.BytesIO()
+    zip_file = zipfile.ZipFile(zip_output, 'w')
+    zip_file.writestr('lambda_function.py', code)
+    zip_file.close()
+    zip_output.seek(0)
+    return zip_output.read()
+
+lambda_client = boto3.client('lambda', region_name='ap-northeast-2')
+
+##################################################################################################
+################################ Create Lambda Function ##########################################
+##################################################################################################
+
+def create_lambda_function(function_name, script, user_env_vars=None):
+    try:
+        zip_file_bytes = create_zip_file(script)
+
+        function_params = {
+            'FunctionName': function_name,
+            'Runtime': 'python3.11',
+            'Role': config('LAMBDA_ROLE_ARN'),
+            'Handler': 'lambda_function.lambda_handler',
+            'Code': {'ZipFile': zip_file_bytes},
+            'Description': 'A Lambda function to run user script.',
+            'Timeout': 60,
+            'MemorySize': 128,
+        }
+
+        if user_env_vars:
+            function_params['Environment'] = user_env_vars
+
+        print(f"Function {function_name} created successfully.")
+        response = lambda_client.create_function(**function_params)
+    except Exception as e:
+        print(f"Error creating function {function_name}: {e}")
+
+    return response
+
+
+def create_lambda_function_url(function_name):
+    try:
+        response = lambda_client.create_function_url_config(
+            FunctionName=function_name,
+            AuthType='NONE'
+        )
+        print(f"Function URL configuration for {function_name} created successfully.")
+        return response
+    except Exception as e:
+        print(f"Error creating URL configuration for {function_name}: {e}")
+
+
+def add_lambda_function_permission(function_name):
+    response = lambda_client.add_permission(
         FunctionName=function_name,
-        Runtime='python3.11',
-        Role='arn:aws:iam::860787305217:user/Xnippet',
-        Handler='lambda_function.lambda_handler',
-        Code={'ZipFile': script},
-        Description='A Lambda function to run user script.',
-        Timeout=60,
-        MemorySize=128,
-        Environment=user_env_vars
+        StatementId='FunctionURLInvokePermission',
+        Action='lambda:invokeFunctionUrl',
+        Principal='*',
+        FunctionUrlAuthType='NONE',
     )
     return response
 
+##################################################################################################
+################################# Get Lambda Function ############################################
+##################################################################################################
+
+def get_lambda_function(function_name):
+    try:
+        response = lambda_client.get_function(FunctionName=function_name)
+        print(f"Function {function_name} found.")
+        return response
+    except Exception as e:
+        print(f"Error finding function {function_name}: {e}")
+
+##################################################################################################
+################################ Update Lambda Function ##########################################
+##################################################################################################
+
+def update_lambda_function_code(function_name, script):
+    try:
+        zip_file_bytes = create_zip_file(script)
+
+        response = lambda_client.update_function_code(
+            FunctionName=function_name,
+            ZipFile=zip_file_bytes,
+        )
+        print(f"Function {function_name} updated successfully.")   
+        return response
+    except Exception as e:
+        print(f"Error updating function {function_name}: {e}")
+
+
+def update_lambda_function_configuration(function_name, user_env_vars):
+    try:
+        response = lambda_client.update_function_configuration(
+            FunctionName=function_name,
+            Environment=user_env_vars
+        )
+        print(f"Function {function_name} updated successfully.")
+        return response
+    except Exception as e:
+        print(f"Error updating function {function_name}: {e}")
+
+
+##################################################################################################
+################################ Delete Lambda Function ##########################################
+##################################################################################################
+
+def delete_lambda_function(function_name):
+    try:
+        response = lambda_client.delete_function(FunctionName=function_name)
+        print(f"Function {function_name} deleted successfully.")
+        return response
+    except Exception as e:
+        print(f"Error deleting function {function_name}: {e}")
+
+
+def delete_lambda_function_url(function_name):
+    try:
+        response = lambda_client.delete_function_url_config(FunctionName=function_name)
+        print(f"Function URL configuration for {function_name} has been deleted.")
+        return response
+    except lambda_client.exceptions.ResourceNotFoundException:
+        print(f"No URL configuration found for {function_name}.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
